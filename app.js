3544575b96c033a0b001eed0ab68650376323bdb
Szymon Bubak <szymon.bubak@contractors.roche.com>
2019-01-14 12:07:17 +0100

    - better waiting function -> it'll wait until elem is ready

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-(function(){
+(function () {
 
-    function replaceDotsWithCommas(string) {
+    var $ = Zepto;
+    var copySourceElemSelector = '#translation_container #action_copy_source';
+    var replaceDotsWithCommas = function (string) {
         return string.replace(/[0-9]+(\.)[0-9]+/g, function (val) {
             return val.replace('.', '{,}')
         })
-    }
+    };
+    var whenElemIsReady = function (selector, cb) {
 
-    var btnOK = setInterval(initializePlugin, 250);
+        if ($(selector).length > 0) {
+            cb();
 
-    function initializePlugin() {
+        } else {
+            setTimeout(function () {
+                whenElemIsReady(selector, cb);
+            }, 100)
+        }
+    };
 
-        var $ = Zepto,
-            $menu = $('#translation_container #action_copy_source').parent(),
+    var initializePlugin = function() {
+
+        $menu = $(copySourceElemSelector).parent(),
             $changeFormatBtn = $('<button tabindex="-1" title="Copy Source & change number format (Ctrl + Alt + N)" class="btn btn-icon"><i class="static-icon-copy"></i></button>'),
             $translation = $('#translation');
 
         if ($menu.length === 0) {
-           return;
+            return;
         }
 
         $changeFormatBtn.css('background', 'url("http://www.glidetraining.com/wp-content/uploads/2015/03/5commastyle.gif") 3px 7px no-repeat');
 
         $menu.append($changeFormatBtn);
 
-        function copyAndReformatNumebrsInTranslation(){
+        function copyAndReformatNumebrsInTranslation() {
             $('#action_copy_source').click();
             $translation.val(replaceDotsWithCommas($translation.val()));
         }
 
         $changeFormatBtn.on('click', copyAndReformatNumebrsInTranslation);
         key('alt+a', copyAndReformatNumebrsInTranslation);
-        clearInterval(btnOK);
-    }
+    };
 
+    whenElemIsReady(copySourceElemSelector, initializePlugin);
 })();
