ed9f0a0e4e1aa7942fa099f27c9572bbc0355438
Daniel Hollas <danekhollas@gmail.com>
2022-07-24 02:19:48 +0100

    Cleanup pack_plugin.sh

--- pack_plugin.sh
+++ pack_plugin.sh
@@ -5,7 +5,8 @@
 # or Firefox
 # https://addons.mozilla.org/en-US/developers/addon/khan-academy-dots/edit
 
-set -eou pipefail
+set -euo pipefail
+set -x
 
 browser=${1-}
 if [[ -z $browser ]];then
@@ -21,52 +22,27 @@ fi
 
 # Don't forget to bump version in manifest.json before each official publish!
 version=$(grep '"version"' manifest.json | awk -F'"' '{print $4}')
-REPO_NAME=KhanAcademyDots
-PACKAGE_NAME=${REPO_NAME}-${version}-${browser}
-
-cd ../ || exit 1
-
-rm -rf $PACKAGE_NAME.zip
-
-if [[ -e $PACKAGE_NAME ]];then
-  rm -r $PACKAGE_NAME
+if [[ -z $version ]];then
+  echo "ERROR: Could not determine version"
+  exit 1
 fi
 
-if [[ ! -d $REPO_NAME ]];then
-   echo "ERROR: Directory $REPO_NAME does not exist!"
-   exit 1
-fi
+PACKAGE_NAME=KhanAcademyDots-${version}-${browser}
 
-if [[ -d $PACKAGE_NAME ]];then
-  rm -rf $PACKAGE_NAME
-fi
+rm -rf $PACKAGE_NAME.zip $PACKAGE_NAME
+mkdir $PACKAGE_NAME
 
-cp -r $REPO_NAME $PACKAGE_NAME
-
-cd $PACKAGE_NAME || exit 1
-if [[ $? -ne 0 ]];then
-   echo "ERROR: Could not enter dir ../$PACKAGE_NAME"
-   exit 1
-fi
+cp *js *html *png *gif manifest.json $PACKAGE_NAME
+cp -r 3rdPartyLibs KhanAcademyLibs $PACKAGE_NAME
 
 # Get rid of module.exports and babel requires,
 # which are not supported in browser plugins
 nlines=$(grep -n 'module.exports' translation-assistant/lib/math-translator.js | awk -F":" '{print $1;end}')
 let nlines--
 head -n $nlines translation-assistant/lib/math-translator.js |\
- egrep -v 'require(.+)' > KhanAcademyLibs/math-translator.js
+ egrep -v 'require(.+)' > $PACKAGE_NAME/KhanAcademyLibs/math-translator.js
 
-rm -rf node_modules/ package.json package-lock.json .eslintrc .git/ *.md pack_plugin.sh translation-assistant/
-if [[ $browser = 'chrome' ]];then
-  # We need to exclude Firefox-specific manifest entries
-  grep -v -e gecko -e browser_specific_settings manifest.json > tmp
-  # More chrome specific things
-  sed -i 's/"persistent": true/"persistent": false/' tmp
-  sed -i 's/browser_style/chrome_style/' tmp
-  mv tmp manifest.json
-fi
-zip -r $PACKAGE_NAME.zip *
-mv $PACKAGE_NAME.zip ../
-cd .. || exit 1
+cd $PACKAGE_NAME
+zip -r ../${PACKAGE_NAME}.zip *
 
-rm -rf $PACKAGE_NAME
+rm -r ../$PACKAGE_NAME
